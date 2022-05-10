d896b8944597ed27c7632f05f3162cc11e729d53
richardmogyoro <richard.mogyoro@gmail.com>
2022-05-10 18:20:00 +0200

    add SetRate method to set values in datatable

--- CurrencyConverter/CurrencyConverter/MainWindow.xaml.cs
+++ CurrencyConverter/CurrencyConverter/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
@@ -28,9 +29,11 @@ namespace CurrencyConverter
         public MainWindow()
         {
             InitializeComponent();
+            ClearControls();
+            BindCurrency();
         }
 
-        private void BindCurrency()
+        private async void BindCurrency()
         {
             DataTable dtCurrency = new DataTable();
             dtCurrency.Columns.Add("Text");
@@ -38,10 +41,20 @@ namespace CurrencyConverter
 
             dtCurrency.Rows.Add("--SELECT--", 0);
             dtCurrency.Rows.Add("EUR", 1);
-            dtCurrency.Rows.Add("USD", 75);
+            GetRates(dtCurrency);
+
+            cmbFromCurrency.ItemsSource = dtCurrency.DefaultView;
+            cmbFromCurrency.DisplayMemberPath = "Text";
+            cmbFromCurrency.SelectedValuePath = "Value";
+            cmbFromCurrency.SelectedIndex = 0;
+
+            cmbToCurrency.ItemsSource = dtCurrency.DefaultView;
+            cmbToCurrency.DisplayMemberPath = "Text";
+            cmbToCurrency.SelectedValuePath = "Value";
+            cmbToCurrency.SelectedIndex = 0;
         }
 
-        private async Task<string> GetRates()
+        private async void GetRates(DataTable dtCurrency)
         {
             var client = new RestClient("https://api.apilayer.com/exchangerates_data/latest?symbols=&base=");
 
@@ -49,28 +62,77 @@ namespace CurrencyConverter
             request.AddHeader("apikey", "orm11MKJCIt2kwBGQiBWOowZgqJdzLSN");
 
             var response = await client.ExecuteAsync(request);
+            SetRates(response.Content, dtCurrency);
+        }
 
-            var details = JObject.Parse(response.Content);
-
-            Console.WriteLine("result:");
-            Console.WriteLine(details["rates"]["CAD"]);
-
-            return response.Content;
+        private void SetRates(string response, DataTable dtCurrency)
+        {
+            var details = JObject.Parse(response);
+            Console.WriteLine(details["rates"]);
+            dtCurrency.Rows.Add("USD", details["rates"]["USD"]);
+            dtCurrency.Rows.Add("AUD", details["rates"]["AUD"]);
+            dtCurrency.Rows.Add("CAD", details["rates"]["CAD"]);
+            dtCurrency.Rows.Add("CHF", details["rates"]["CHF"]);
+            dtCurrency.Rows.Add("GBP", details["rates"]["GBP"]);
+            dtCurrency.Rows.Add("JPY", details["rates"]["JPY"]);
+            dtCurrency.Rows.Add("HUF", details["rates"]["HUF"]);
         }
 
         private void Convert_Click(object sender, RoutedEventArgs e)
         {
+            double ConvertedValue;
 
+            if (txtCurrency.Text == null || txtCurrency.Text.Trim() == "")
+            {
+                MessageBox.Show("Please Enter Currency", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                txtCurrency.Focus();
+                return;
+            }
+            else if (cmbFromCurrency.SelectedValue == null || cmbFromCurrency.SelectedIndex == 0)
+            {
+                MessageBox.Show("Please Select Currency From", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                cmbFromCurrency.Focus();
+                return;
+            }
+            else if (cmbToCurrency.SelectedValue == null || cmbToCurrency.SelectedIndex == 0)
+            {
+                MessageBox.Show("Please Select Currency To", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                cmbToCurrency.Focus();
+                return;
+            }
+
+            if (cmbFromCurrency.Text == cmbToCurrency.Text)
+            {
+                ConvertedValue = double.Parse(txtCurrency.Text);
+                lblCurrency.Content = cmbToCurrency.Text + " " + ConvertedValue.ToString("N3");
+            }
+            else
+            {
+                ConvertedValue = (double.Parse(cmbToCurrency.SelectedValue.ToString()) / (double.Parse(cmbFromCurrency.SelectedValue.ToString()) * double.Parse(txtCurrency.Text)));
+                lblCurrency.Content = cmbToCurrency.Text + " " + ConvertedValue.ToString("N3");
+            }
         }
 
         private void Clear_Click(object sender, RoutedEventArgs e)
         {
-            lblCurrency.Content = "";
+            ClearControls();
         }
 
-        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        private void ClearControls()
         {
-            
+            txtCurrency.Text = string.Empty;
+            if (cmbFromCurrency.Items.Count > 0)
+                cmbFromCurrency.SelectedIndex = 0;
+            if (cmbToCurrency.Items.Count > 0)
+                cmbToCurrency.SelectedIndex = 0;
+            lblCurrency.Content = "";
+            txtCurrency.Focus();
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e) //Allow Only Integer in Text Box
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
         }
     }
 }
